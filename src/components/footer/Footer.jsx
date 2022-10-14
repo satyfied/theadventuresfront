1398d5301312880b459cf3eeae3c33c84cd38c94
satyfied <handxy02@gmail.com>
2022-10-14 17:47:01 +0100

    updat

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#services">Service</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#portfolio">Gallery</a></li>
         <li><a href="#testimonials">Testimonials</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
