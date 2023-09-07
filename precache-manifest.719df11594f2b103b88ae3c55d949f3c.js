8695589185e5f3e39b67c94d42cd7f2c750a748f
satyfied <handxy02@gmail.com>
2023-09-07 16:32:13 +0100

    Updates

---
+++ precache-manifest.719df11594f2b103b88ae3c55d949f3c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "0025d87134441f029e0c",
+    "url": "/static/css/main.ed01b6db.chunk.css"
+  },
+  {
+    "revision": "0025d87134441f029e0c",
+    "url": "/static/js/main.0025d871.chunk.js"
+  },
+  {
+    "revision": "44c8513cdf965bf47be4",
+    "url": "/static/css/1.196dab1c.chunk.css"
+  },
+  {
+    "revision": "44c8513cdf965bf47be4",
+    "url": "/static/js/1.44c8513c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e02c2c640eab2381248992da2f39465b",
+    "url": "/static/media/AVATTomi.e02c2c64.jpg"
+  },
+  {
+    "revision": "69adf240359bece3b0b759b9c636bf0c",
+    "url": "/static/media/Anima2.69adf240.gif"
+  },
+  {
+    "revision": "554549ffdb89876bd0f7a4f26847133a",
+    "url": "/static/media/AOA.554549ff.jpg"
+  },
+  {
+    "revision": "e8b47b3a168653b4869266f3f30d2c9e",
+    "url": "/static/media/Anima.e8b47b3a.gif"
+  },
+  {
+    "revision": "b296ccc02ffa3d8db3a74046e4d8825f",
+    "url": "/static/media/Gallery1.b296ccc0.gif"
+  },
+  {
+    "revision": "4d46827fe1e02f54305bfe979d105eed",
+    "url": "/static/media/Gallery2.4d46827f.gif"
+  },
+  {
+    "revision": "4cc8b3728b3946fd8b0ad8f84bdbf293",
+    "url": "/static/media/Gallery3.4cc8b372.gif"
+  },
+  {
+    "revision": "04799776ae0df36e3a407d40b731cc27",
+    "url": "/static/media/Gallery4.04799776.gif"
+  },
+  {
+    "revision": "f365cab2310ce729e2eeebf4cbd7816f",
+    "url": "/static/media/Gallery5.f365cab2.gif"
+  },
+  {
+    "revision": "58b6b3a84f744bffcefee0dbe77a1ea3",
+    "url": "/static/media/Gallery6.58b6b3a8.gif"
+  },
+  {
+    "revision": "36ba959fbf5491f7ca4a2ada3c4f8758",
+    "url": "/static/media/AVATLekan.36ba959f.jpg"
+  },
+  {
+    "revision": "c37e79991514191c8e683be3941be545",
+    "url": "/static/media/AVATDavid.c37e7999.jpg"
+  },
+  {
+    "revision": "f8018e3f276deff348befff642ef1ba6",
+    "url": "/static/media/bg-reading.f8018e3f.png"
+  },
+  {
+    "revision": "eab554b07c99283723a9e11f0a465565",
+    "url": "/index.html"
+  }
+];
